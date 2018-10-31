3044a848d72dbcb8c25e0aa8206a12c5c03bb9c0
Ranc58 <rancvova@gmail.com>
2018-10-31 16:51:19 +0300

    Del unused queryset manager

--- src/meteo_api/models.py
+++ src/meteo_api/models.py
@@ -5,23 +5,7 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
-class ForecastQuerySet(models.QuerySet):
-
-    def forecasts_by_period(self, days, forecast_start_date):
-        if days > 7:
-            days = 7
-        elif days <= 0:
-            days = 3
-        max_date = forecast_start_date + timedelta(days=days)
-        queryset = Forecast.objects.filter(
-            forecast_datetime__gte=forecast_start_date,
-            forecast_datetime__lte=max_date
-        )
-        return queryset
-
-
 class Forecast(models.Model):
-    objects = ForecastQuerySet.as_manager()
     temperature = models.FloatField(verbose_name=_('Temperature'))
     forecast_datetime = models.DateTimeField(verbose_name=_('Forecast datetime'))
     created_dttm = models.DateTimeField(auto_now_add=True)
